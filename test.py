1fc68453f50dd8822716b29b09c823c8612c3fc8
alessandroPoggiali <alessandro.poggiali97@gmail.com>
2022-10-03 10:59:02 +0200

    new test

--- test.py
+++ test.py
@@ -647,7 +647,7 @@ if __name__ == "__main__":
         'max_iterations': [5]
     }
     
-    dataset = Dataset('iris', '2-norm')
+    dataset = Dataset('iris', 'inf-norm')
     
     print("---------------------- " + str(dataset.dataset_name) + " Test ----------------------\n")
     
